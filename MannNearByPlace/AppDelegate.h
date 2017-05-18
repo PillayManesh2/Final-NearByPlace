a23c629eed7f8a77aa64a3e6595e2dfb1d9e2014
Student 5 <student5@Felix-P-05.local>
2017-05-18 14:36:58 +0530

    Initial Commit

---
+++ MannNearByPlace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MannNearByPlace
+//
+//  Created by Student 5 on 18/05/17.
+//  Copyright © 2017 Student 5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MannNearByPlace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MannNearByPlace
+//
+//  Created by Student 5 on 18/05/17.
+//  Copyright © 2017 Student 5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
